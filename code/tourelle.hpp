9a137f16ec543a4df086e0671fddbc1ac596ac50
martin <martin@pc.lri.fr>
2015-05-04 12:37:15 +0200

    modification code arnaud 2

--- code/tourelle.hpp
+++ code/tourelle.hpp
@@ -6,11 +6,11 @@
 class Tourelle
 {
     public :
-    Tourelle(size_t degatsAttaque_, Case& case_) : degatsAttauque(degatsAttaque_), saCase(case_) {}
+    Tourelle(size_t degatsAttaque_, Case& case_) : degatsAttaque(degatsAttaque_), saCase(case_) {}
     Tourelle(const Tourelle& tour) : degatsAttaque(tour.degatsAttaque),saCase(tour.saCase) {}
 
-    auto getDegatsAttaque() const { return degatsAttaque; }
-    auto& getCase(){ return saCase; }
+    size_t getDegatsAttaque() const { return degatsAttaque; }
+    Case& getCase(){ return saCase; }
 
     private :
     size_t degatsAttaque;
