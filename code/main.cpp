34fd4a57d82d82515a3814e0d0b054d29e7c59e0
martin <martin@pc.lri.fr>
2015-07-09 10:51:45 +0200

    l'architecture avec thoth et hoop directement dans le projet est pas bonne

--- code/main.cpp
+++ code/main.cpp
@@ -15,12 +15,32 @@
 #include "testPourLeDev.hpp"
 using namespace std;
 
+#include <SFML/Graphics.hpp>
+#include "thoth/window.hpp"
 
-int main ()
+int main()
 {
+    thoth::window window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     TestClass::testDijkstra();
     exempleIterateurFoncteur();
     exempleParamInfo();
 
+    return 0;
 }
